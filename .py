9da217af7cd7b28af8a37ee1ffce7fc4b233bd0a
Nishanth S J <nishanthsj93@gmail.com>
2025-12-03 23:41:09 +0530

    Implement factorial and sum functions
    Added functions to calculate factorial and sum of natural numbers.

--- .py
+++ .py
@@ -1,15 +1,17 @@
-# fibbonacci numbers
-def fibo(n):
-  num1=0
-  num2=1
-  next_number=num2
-  count=2
-  print(num1,"",num2,end="")
-  while count<n:
-    print(next_number,end="")
-    count+=1
-    num1,num2=num2,next_number
-    next_number=num1=num2
-n=int(input("enter the limit"))
-fibo(n)
-print()
+
+#python program to find factorial of given number and sum of natural numbers
+def fact(n):
+   return 1 if (n==1 or n==0)else n*fact(n-1);
+def sum(n):
+   return 0 if (n==0)else (n-1);
+num=int(input("enter any number:"))
+print("1-to find the factorial,2-to find the sum 3-exit")
+opt=int(input("enter the option 1-3:"))
+if(opt==1):
+   print("factorial of ",num,"is:",fact(num))
+else:
+    if(opt==2):
+     print("sum of",num,"is:",sum(num))
+     else:
+         print("")
+           
